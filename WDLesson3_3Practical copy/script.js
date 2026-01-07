4444634ab604e441d16badb90850a3597a93fc31
Gabriel247-Luck <gabrield157@nycstudents.net>
2026-01-07 00:49:34 +0000

    HELLO

---
+++ WDLesson3_3Practical copy/script.js
@@ -0,0 +1,20 @@
+/* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function Hamburger(){
+    document.getElementById("highlight").innerHTML="Hamburger";
+}
+
+function Sandwhich(){ 
+    document.getElementById("highlight").innerHTML="Sandwhich";
+}
+
+function Pizza(){ 
+    document.getElementById("highlight").innerHTML="Pizza";
+}
+
+function Hulks_Logo(){ 
+    document.getElementById("highlight").innerHTML="Hulks Logo";
+}
+
+function Circle(){ 
+    document.getElementById("highlight").innerHTML="Circle";
+}
