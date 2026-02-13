4ac59525f30710c3af42c0a2cca06a487ca45a48
Gabriel247-Luck <gabrield157@nycstudents.net>
2026-02-13 19:04:37 +0000

    HELLO

--- WDLesson4_3Practical/script.js
+++ WDLesson4_3Practical/script.js
@@ -14,3 +14,31 @@ Guideline:
 Use past examples, classwork and practicals to assist you in completing this practical.
 */
 
+function BMI() {
+  let weight = parseFloat(document.getElementById("Wt").value);
+  let height = parseFloat(document.getElementById("Ht").value);
+  let output = document.getElementById("output");
+  let filename="";
+  let message="";
+  let BMI = 703 * weight / (height**2);
+
+  if (BMI < 18.5){
+       message= "Underweight";
+       filename= "underweight.png";
+
+  }else if (BMI >= 18.5 &&  BMI < 25){
+       message= "Healthy weight";
+       filename= "healthyweight.png";
+
+  }else if (BMI >= 25.0 && BMI < 30){
+     message= "Overweight";
+     filename= "overweight.png";
+
+  }else if (BMI >= 30){
+     message= "Obese";
+     filename= "obeseweight.png";
+  }
+
+  output.innerHTML= `<img src="${filename}"><br>You are ${message}`;
+
+}
